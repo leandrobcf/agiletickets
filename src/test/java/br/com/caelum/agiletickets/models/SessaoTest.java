550061b514a88e3ac66c4f05db43f183a3747d71
leandro <leandrobcf@gmail.com>
2011-12-06 12:01:42 -0200

    teste na classe sessao

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -1,33 +1,50 @@
 package br.com.caelum.agiletickets.models;
 
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SessaoTest {
 
-
-	@Test
-	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-        sessao.setTotalIngressos(2);
-
-        Assert.assertTrue(sessao.podeReservar(1));
+	private Sessao s;
+	
+	@Before
+	public void inicializa() {
+		s = new Sessao();
 	}
 
 	@Test
-	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(2);
+	public void deveVender1ingressoSeHa2vagas() throws Exception {		
+        s.setTotalIngressos(2);
 
-		Assert.assertFalse(sessao.podeReservar(3));
+        Assert.assertTrue(s.podeReservar(1));
+	}
+
+	
+
+	@Test
+	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
+		
+		s.setTotalIngressos(2);
+
+		Assert.assertFalse(s.podeReservar(3));
 	}
 
 	@Test
 	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(5);
+	
+		s.setTotalIngressos(5);
 
-		sessao.reserva(3);
-		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
+		s.reserva(3);
+		Assert.assertEquals(2, s.getIngressosDisponiveis().intValue());
+	}
+	
+	@Test
+	public void devePoderReservarTodosIngressosDisponiveis() throws Exception {
+		
+		s.setTotalIngressos(5);
+		
+		Assert.assertTrue(s.podeReservar(5));
 	}
 }
